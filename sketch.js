3ed8677dc171778e96f3416c49dbf19e0385a2ea
Sounak Ghosh <sounaktheghosh@gmail.com>
2020-11-04 13:40:47 -0500

    added styling, dom elements, changed model

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 var textInput;
 var textPosY = 200;
+let infoText;
 let generateButton;
 let bg = 250;
-var outputText;
+let generatedText = '';
+let bubbles = [];
 
 
 // Create the character level generator with a pre trained model
-const rnn = ml5.charRNN('models/woolf/', modelReady);
+const rnn = ml5.charRNN('models/zora_neale_hurston/', modelReady);
 
 // When the model is loaded
 function modelReady() {
@@ -15,31 +17,73 @@ function modelReady() {
 }
 
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(600, 500);
+    
+    //create DOM elements
+    infoText = createP('Write a line, and let Compoeter complete the rhyme!');
+    infoText.position(100,30)
   
     textInput = createInput();
-  
     textInput.size(400, 25)
-    textInput.position(width/2 - textInput.width/2, 50);
+    textInput.position(100, 80);
   
     generateButton = createButton('loading...');
-    generateButton.position(width/2 - generateButton.width/2, 100);
+    generateButton.position(385, 120);
     generateButton.mousePressed(generateText);
+    
+    tempSlider = createSlider(0, 1, 0.5, 0.1);
+    tempSlider.position(130 + generateButton.width, 122);
+    tempSlider.size(150,25);
+    
+    sliderL = createP('Temperature: ');
+    sliderL.position(100,110);
+    
+    credit = createP('Created using ml5 library, pre-trained model from corpus of Zora Neale Hurston, and RiTa library. ')
+    credit.class('plain-text');
+    
+    //bubbles animation
+    for (let i=0; i<20; i++) {
+    bubbles.push({
+          speed: random(.1, 0.7),
+          clr: color(random(0, 60), random(80, 150), random(100, 240)),
+          x: width/2 + random(-200, 200),
+          y: height - 25,
+          size: random(5, 15)
+        });
+    }
 
 }
 
 function draw() {
     background(bg);
+    
+    for (let bubble of bubbles) {
+
+    noStroke();
+    fill(bubble.clr);
+    ellipse(bubble.x, bubble.y, bubble.size);
+    
+    bubble.y -= bubble.speed;
+    bubble.size -= .1;
+    
+    if (bubble.size < .5) {
+      bubble.size = random(7, 20);
+      bubble.y = height;
+      bubble.speed = random(.1, 0.7);
+    }
+  }
 }
 
 function generateText () {
+    //change background
+    bg = color(random(220,255),random(220,255),random(220,255));
     
-    bg = color(random(180,255),random(180,255),random(180,255));
-    
+    //add input text
     newLine = createP();
     newLine.position(100, textPosY);
     newLine.html(textInput.value());
     
+    //compoeter writing...
     genLine = createP('...');
     genLine.position(100, textPosY+20);
     
@@ -49,30 +93,37 @@ function generateText () {
     console.log(numChars);
     
     var words = rs.words();
-    var generatedText = '';
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-        
-        if (!words[0].isNoun) {
-            console.log(words[0])
-        }
+    
+    //rhyming the last word
+    let wordToRhyme;
+    //ignore punctuation
+    if (RiTa.isPunctuation(words[words.length-1])) {
+        wordToRhyme = words[words.length-2];
+    } else {
+        wordToRhyme = words[words.length-1];
     }
+    //console.log(wordToRhyme);
+    
+    let newRhymes = RiTa.rhymes(wordToRhyme);
+//    console.log(newRhymes);
+//    console.log(newRhymes.length-1);
+    
+    var endWord = newRhymes[Math.floor(Math.random() * newRhymes.length)];
+//    console.log(endWord);
     
     if (rnn.ready) {
-        //console.log("Running", textInput.value());
-        // Generate content
-        rnn.generate({ seed: textInput.value(), length: numChars, temperature: .8 }, (err, results) =>     {
+        
+        rnn.generate({ seed: textInput.value(), length: numChars, temperature: tempSlider.value() }, (err, results) =>     {
             
             generatedText = results;
-            console.log(results);
-            genLine.html(generatedText.sample);
+            
+            console.log(generatedText);
+            genLine.html(generatedText.sample + ' ' + endWord);
             
         });
-        
+        console.log(generatedText.sample);
     }
-    
+
     textPosY += 40;
-    
-    
 
 }
